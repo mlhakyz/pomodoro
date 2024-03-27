ed58070ee895f300ddb7cf42915c25953ae72a64
mlhakyz <mlhakyz81@gmail.com>
2024-03-27 12:52:26 +0300

    Refresh button added and icon color changed.

--- app/src/main/java/com/mlhakyz/pomodoro/MainActivity.kt
+++ app/src/main/java/com/mlhakyz/pomodoro/MainActivity.kt
@@ -20,9 +20,13 @@ class MainActivity : AppCompatActivity() {
     private val timeFormat: SimpleDateFormat = SimpleDateFormat("mm:ss")
     private var isTimerRunning: Boolean = false
     private var timer: CountDownTimer? = null
-    private var timeLeftInMillis: Long = 1500000 // 25 dakika
+    private var pomodoroTimeMills: Long = 1500000 // 25 dakika
+    private var shortPauseTimeMills: Long = 300000 // 5 dakika
+    private var longPauseTimeMills: Long = 900000 // 15 dakika
+    private var selectedTimeInMillis: Long = 0
     private lateinit var lftMediumFont: Typeface
     private lateinit var lftBoldFont: Typeface
+    private var timeLeftInMillis: Long = 0 // Kaldığı yerden devam etmek için tutulan süre
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,96 +36,114 @@ class MainActivity : AppCompatActivity() {
 
         lftMediumFont = ResourcesCompat.getFont(this, R.font.ltfmedium)!!
         lftBoldFont = ResourcesCompat.getFont(this, R.font.ltfbold)!!
-
-
+        selectedTimeInMillis = pomodoroTimeMills
     }
 
-        fun pomodoroOnClick(view:View){
+    fun pomodoroOnClick(view:View){
+            timer?.cancel()
+            timeLeftInMillis = 0
             // Button 1'e tıklandığında yapılacak işlemler
             binding.pomodoroBtn.setBackgroundColor(resources.getColor(R.color.golge))
             binding.pomodoroBtn.typeface = lftBoldFont
+            binding.textView.text = "25:00"
+            selectedTimeInMillis = pomodoroTimeMills
+        pauseTimer()
+            //Arka Plan ve Count Down Timer Ayarlama
+            binding.main.setBackgroundColor(getColor(R.color.pomodoroColor))
+            binding.startPauseBtn.setTextColor(getColor(R.color.pomodoroColor))
 
             // Diğer düğmelerin arka plan rengini varsayılan değere döndür
-            binding.shortBreakBtn.setBackgroundColor(getColor(R.color.pomodoroColor))
+        binding.refreshBtn.iconTint = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.pomodoroColor))
+
+        binding.shortBreakBtn.setBackgroundColor(getColor(R.color.pomodoroColor))
             binding.longBreakBtn.setBackgroundColor(getColor(R.color.pomodoroColor))
             binding.shortBreakBtn.typeface = lftMediumFont
             binding.longBreakBtn.typeface = lftMediumFont
 
-            //Arka Plan ve Count Down Timer Ayarlama
-            binding.main.setBackgroundColor(getColor(R.color.pomodoroColor))
-            binding.startPauseBtn.setTextColor(getColor(R.color.pomodoroColor))
-        }
-
-
+    }
 
     fun shortBreakOnClick(view: View){
+        timer?.cancel()
+        timeLeftInMillis = 0
         // Button 2'ye tıklandığında yapılacak işlemler
         binding.shortBreakBtn.setBackgroundColor(resources.getColor(R.color.golge))
         binding.shortBreakBtn.typeface = lftBoldFont
+        binding.textView.text = "05:00"
+        selectedTimeInMillis = shortPauseTimeMills
+        pauseTimer()
+        //Arka Plan ve Count Down Timer Ayarlama
+        binding.main.setBackgroundColor(getColor(R.color.shortPauseColor))
+        binding.startPauseBtn.setTextColor(getColor(R.color.shortPauseColor))
 
         // Diğer düğmelerin arka plan rengini varsayılan değere döndür
+        binding.refreshBtn.iconTint = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.shortPauseColor))
+
         binding.pomodoroBtn.setBackgroundColor(getColor(R.color.shortPauseColor))
         binding.longBreakBtn.setBackgroundColor(getColor(R.color.shortPauseColor))
         binding.pomodoroBtn.typeface = lftMediumFont
         binding.longBreakBtn.typeface = lftMediumFont
 
-        //Arka Plan ve Count Down Timer Ayarlama
-        binding.main.setBackgroundColor(getColor(R.color.shortPauseColor))
-        binding.startPauseBtn.setTextColor(getColor(R.color.shortPauseColor))
 
     }
 
     fun longBreakOnClick(view: View){
+        timer?.cancel()
+        timeLeftInMillis = 0
         // Button 3'e tıklandığında yapılacak işlemler
         binding.longBreakBtn.setBackgroundColor(resources.getColor(R.color.golge))
         binding.longBreakBtn.typeface = lftBoldFont
+        binding.textView.text = "15:00"
+        selectedTimeInMillis = longPauseTimeMills
+      pauseTimer()
+        //Arka Plan ve Count Down Timer Ayarlama
+        binding.main.setBackgroundColor(getColor(R.color.longPauseColor))
+        binding.startPauseBtn.setTextColor(getColor(R.color.longPauseColor))
 
         // Diğer düğmelerin arka plan rengini varsayılan değere döndür
+        binding.refreshBtn.iconTint = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.longPauseColor))
+
         binding.pomodoroBtn.setBackgroundColor(getColor(R.color.longPauseColor))
         binding.shortBreakBtn.setBackgroundColor(getColor(R.color.longPauseColor))
         binding.pomodoroBtn.typeface = lftMediumFont
         binding.shortBreakBtn.typeface = lftMediumFont
 
 
-        //Arka Plan ve Count Down Timer Ayarlama
-        binding.main.setBackgroundColor(getColor(R.color.longPauseColor))
-        binding.startPauseBtn.setTextColor(getColor(R.color.longPauseColor))
+    }
 
+    fun btnOnStartStop(view: View) {
+        if (!isTimerRunning) {
+
+            startTimer(if (timeLeftInMillis.toInt() == 0) selectedTimeInMillis else timeLeftInMillis)
+            binding.refreshBtn.visibility = View.VISIBLE
+        } else {
+                pauseTimer()
+        }
     }
 
 
-
-        fun btnOnStartStop(view: View) {
-            if (!isTimerRunning) {
-                startTimer()
-            } else {
-                pauseTimer()
-            }
-        }
-
-
-    private fun startTimer(){
-        timer = object : CountDownTimer(timeLeftInMillis, 1000) {
+    private fun startTimer(durationInMillis: Long){
+        timer = object : CountDownTimer(durationInMillis, 1000) {
             override fun onTick(p0: Long) {
-                timeLeftInMillis = p0
+                timeLeftInMillis = p0 // Geri sayım süresini güncelle
                 binding.textView.text = timeFormat.format(p0)
             }
 
             override fun onFinish() {
                 binding.textView.text = "00:00"
                 isTimerRunning = false
-                binding.startPauseBtn.text = "Start"
+                binding.startPauseBtn.text = "START"
             }
         }.start()
 
         isTimerRunning = true
-        binding.startPauseBtn.text = "Pause"
+        binding.startPauseBtn.text = "PAUSE"
 
     }
     private fun pauseTimer() {
         timer?.cancel()
         isTimerRunning = false
-        binding.startPauseBtn.text = "Start"
+        binding.startPauseBtn.text = "START"
+        binding.refreshBtn.visibility = View.INVISIBLE
     }
 
 }
